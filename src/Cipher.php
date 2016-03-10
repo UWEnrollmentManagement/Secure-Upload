609f454d0a1f86926660078fc60f615ac2822e12
Joseph Schilz <joseph@schilz.org>
2016-03-10 10:26:09 -0800

    Initial commit.

---
+++ src/Cipher.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace UWDOEM\SecureUploads;
+
+
+class Cipher
+{
+
+    /**
+     * Encrypt a file specified in the $_FILES global.
+     *
+     * @param string $fileHandle
+     * @param string $destination
+     * @param string $publicKeyLocation
+     * @return string
+     */
+    public static function encrypt($fileHandle, $destination, $publicKeyLocation)
+    {
+        /** @var resource $publicKey */
+        $publicKey = openssl_get_publickey(file_get_contents($publicKeyLocation));
+
+        /** @var string $data */
+        $data = file_get_contents($_FILES[$fileHandle]['tmp_name']);
+        /** @var string $info */
+        $info = json_encode($_FILES[$fileHandle]);
+
+        openssl_seal(gzcompress($data), $encryptedData, $dataKeys, [$publicKey]);
+        openssl_seal(gzcompress($info), $encryptedInfo, $infoKeys, [$publicKey]);
+
+        /** @var string $hash */
+        $hash = md5($encryptedData);
+
+        file_put_contents("$destination/$hash.data", $encryptedData);
+        file_put_contents("$destination/$hash.info", $encryptedInfo);
+
+        file_put_contents("$destination/$hash.data.key", $dataKeys[0]);
+        file_put_contents("$destination/$hash.info.key", $infoKeys[0]);
+
+        openssl_free_key($publicKey);
+
+        return "$destination/$hash.data";
+    }
+
+    /**
+     * @param string $filePath
+     * @param string $destination
+     * @param string $privateKeyLocation
+     * @return string
+     * @throws \Exception if ::decrypt is not able to decrypt the files.
+     */
+    public static function decrypt($filePath, $destination, $privateKeyLocation)
+    {
+        /** @var string $dir */
+        $dir = dirname($filePath);
+        /** @var string $hash */
+        $hash = basename($filePath, ".data");
+
+        /** @var resource $privateKeyId */
+        $privateKeyId = openssl_get_privatekey(file_get_contents($privateKeyLocation));
+
+        /** @var string $encryptedData */
+        $encryptedData = file_get_contents($filePath);
+        /** @var string $encryptedInfo */
+        $encryptedInfo = file_get_contents("$dir/$hash.info");
+
+        /** @var string $dataKey */
+        $dataKey = file_get_contents("$dir/$hash.data.key");
+        /** @var string $dataKey */
+        $infoKey = file_get_contents("$dir/$hash.info.key");
+
+        /** @var boolean $dataWasDecrypted */
+        $dataWasDecrypted = openssl_open($encryptedData, $decryptedData, $dataKey, $privateKeyId);
+        /** @var boolean $infoWasDecrypted */
+        $infoWasDecrypted = openssl_open($encryptedInfo, $decryptedInfo, $infoKey, $privateKeyId);
+
+        if ($dataWasDecrypted && $infoWasDecrypted) {
+            /** @var string[] $info */
+            $info = json_decode(gzuncompress($decryptedInfo), true);
+
+            /** @var string $filename */
+            $filename = preg_replace(
+                    '/[^A-Za-z0-9_\-]/', '_',
+                    htmlentities(pathinfo($info['name'], PATHINFO_FILENAME))
+                ) . '.' . preg_replace(
+                    '/[^A-Za-z0-9_\-]/', '_',
+                    htmlentities(pathinfo($info['name'], PATHINFO_EXTENSION))
+                );
+
+            file_put_contents("$destination/$filename", gzuncompress($decryptedData));
+        } else {
+            throw new \Exception('Failed to decrypt file.');
+        }
+
+        openssl_free_key($privateKeyId);
+
+        return "$destination/$filename";
+    }
+
+}
---
+++ test/CipherTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Athens\Encryption\Test;
+
+use PHPUnit_Framework_TestCase;
+
+use UWDOEM\SecureUploads\Cipher;
+
+
+class CipherTest extends PHPUnit_Framework_TestCase
+{
+    public function testEncryptDecrypt()
+    {
+        $word = str_shuffle('abcdef');
+
+        $data = str_repeat($word, rand(10, 30));
+        for($i = 0; $i < 100; $i++) {
+            $data .= str_shuffle('abcdefeghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
+        }
+
+        $filename = md5(rand()) . '.txt';
+        $fileLocation = __DIR__ . '/tmp/' . $filename;
+
+        file_put_contents($fileLocation, $data);
+
+        $_FILES['test-data'] = [
+            'tmp_name' => $fileLocation,
+            'name' => $filename,
+        ];
+
+        $location = Cipher::encrypt('test-data', __DIR__ . '/tmp', __DIR__ . '/certs/publickey.cer');
+
+        if (!is_dir(__DIR__ . '/tmp')) {
+            mkdir(__DIR__ . '/tmp');
+        }
+
+        if (!is_dir(__DIR__ . '/tmp/out')) {
+            mkdir(__DIR__ . '/tmp/out');
+        }
+
+        Cipher::decrypt($location, __DIR__ . '/tmp/out/' , __DIR__ . '/certs/privatekey.pem');
+
+        // Delete everything in the tmp directory
+        array_map('unlink', glob(__DIR__ . "/tmp/out/*"));
+        rmdir(__DIR__ . '/tmp/out');
+        array_map('unlink', glob(__DIR__ . "/tmp/*"));
+        rmdir(__DIR__ . '/tmp');
+    }
+}
